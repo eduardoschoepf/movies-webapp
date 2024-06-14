ffdaf8031c50f74770daa06070e4f7260f384107
eschoepf <eduardo.schoepf@hotmail.com>
2024-06-14 12:35:19 +0200

    first commit

---
+++ src/test/java/com/webapp/moviesapp/MoviesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.moviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
